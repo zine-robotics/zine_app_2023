20204b1d920c87f49040e320623bb1e79ac2b7e9
RUPESH <89046490+rupesh031@users.noreply.github.com>
2023-03-29 07:59:31 +0530

    registration check, timeLine Events fetch

--- lib/screens/explore/view_model/timeline_vm.dart
+++ lib/screens/explore/view_model/timeline_vm.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/cupertino.dart';
+import 'package:zineapp2023/models/events.dart';
+import 'package:zineapp2023/screens/dashboard/repo/dash_repo.dart';
+import 'package:zineapp2023/screens/events/repo/events_repo.dart';
 
-class TimelineVm extends ChangeNotifier{
+class TimelineVm extends ChangeNotifier {
+  final EventsRepo eventRepo = EventsRepo();
+  List<dynamic> listEvents = [];
+  var prev=0;
+  void getStagesEvents() async {
+    print("called");
+    List<dynamic> list = await eventRepo.getEvents();
+    listEvents=[];
+    for (int i = 1; i <= 6; i++) {
+
+      List<dynamic>? subList = list.where((element) => element.stage == i)
+          .toList();
+      if (subList != null) {
+        listEvents.add(subList);
+        // if(listEvents[i].length>0)
+        // {
+        //   print(listEvents[i][0].name);
+        // }
+      }
+      else {
+        listEvents.add([]);
+      }
+    }
+    print(listEvents);
+    if(prev!=listEvents.length) {
+      prev=listEvents.length;
+      notifyListeners();
+    }
+      }
+
 
 }
+
+
+
